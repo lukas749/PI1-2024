1ebee1374787a90876a0903a85be3e62525d1138
Lukáš Mäkký <lukas896698@gmail.com>
2024-10-16 19:46:41 +0200

    add.DU

---
+++ D.U/04-priklady5.py
@@ -0,0 +1,3 @@
+bajt = int(input("Zadaj počet bajtov"))
+maxh = (bajt **256 -1)
+print (f"maximálna hodnota je: {maxh} bajtov")
---
+++ D.U/04-priklady6.py
@@ -0,0 +1,3 @@
+číslo1 = float(input("Zadaj prvé číslo:"))
+číslo2 = float(input("Zadaj druhé číslo:"))
+print (číslo1 + číslo2)
