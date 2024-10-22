cd303c1619f835eec1df3d145653fa369a696c27
Lukáš Mäkký <lukas896698@gmail.com>
2024-10-22 17:32:29 +0200

    add.DU

--- D.U/04-priklady4.py
+++ D.U/04-priklady4.py
@@ -1,6 +1,11 @@
-rok = 365
-mesiac = 12 * 365
-deň = 365 * 12 * 30
-hodina = 365 * 12 * 30 * 24
-minúta = 365 * 12 * 30 * 24 *60
-sekunda = 365 * 12 * 30 *24 * 60 * 60
+R = int(input("Aký je rok:"))
+M = int(input("Aký je mesiac:"))
+D = int(input("Aký je deň v mesiaci je:"))
+a = ((R * 365) + (M * 30) + D) - 2009 * 365
+b = a * 24
+c = b **60
+print(f"Od zavedenia eura uplynulo: {a} dní")
+print(f"Od zavedenia eura uplynulo: {b} dní")
+print(f"Od zavedenia eura uplynulo: {c} sekúnd")
+
+
